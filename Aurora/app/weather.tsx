5d7d65c16b0593d4f6a9d8f11dee4e2645c7cf9c
Samvit Prakash <samvitprakash2004@gmail.com>
2026-01-22 14:31:42 +0200

    feat: update weather variable to "Windy", add SideButton component to Weather, and refactor styles for dynamic positioning

--- Aurora/app/weather.tsx
+++ Aurora/app/weather.tsx
@@ -3,10 +3,12 @@ import { useRef } from 'react';
 import { router, useLocalSearchParams } from 'expo-router';
 
 import  cssAdapter from '@/styles/pages/weather';
+import SideButton from '@/components/side_button/SideButton';
 
 export default function Weather() {
     const { weather } = useLocalSearchParams();
     const styles = cssAdapter(weather);
+    const weatherStr = Array.isArray(weather) ? weather[0] : (weather ?? '');
 
     const onPanResponder = useRef(
         PanResponder.create({
@@ -24,7 +26,7 @@ export default function Weather() {
 
     return (
         <View style={styles.PAGE_CONTAINER} {...onPanResponder.panHandlers}>
-            
+            <SideButton weatherVariable={weatherStr} page={""} icon={"arrow-right"} top={"10%"} invert={true} right={true}/>
 
         </View>
     );
---
+++ Aurora/components/hourly_weather_card/HourlyWeatherCard.tsx
@@ -0,0 +1,9 @@
+import { View, Text }from 'react-native'
+
+export default function HourlyWeatherCard() {
+	return (
+		<View>
+			<Text>Hourly Weather</Text>
+		</View>
+	);
+}
