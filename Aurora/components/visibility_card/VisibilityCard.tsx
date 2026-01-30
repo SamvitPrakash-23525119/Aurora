887956495fdb24bc07a57d969af701e3fbcc65f5
Samvit Prakash <samvitprakash2004@gmail.com>
2026-01-31 00:46:09 +0200

    feat: add VisibilityCard component with styling and integrate into Metrics, update weatherVariable state in Index

---
+++ Aurora/components/visibility_card/VisibilityCard.tsx
@@ -0,0 +1,22 @@
+import { VisibilityIcon } from '@/components/illustrations/VisibilityIcon';
+import cssAdapter from '@/styles/components/visibilityCard';
+import colors from '@/styles/global/colors';
+import { Text, View } from 'react-native';
+
+export default function VisibilityCard({ weatherVariable, visibility }: {weatherVariable: string, visibility: string}) {
+    const styles = cssAdapter(weatherVariable);
+    const colorScheme = colors(weatherVariable);
+
+    return (
+        <View style={styles.CARD_CONTAINER}>
+            <View style={styles.HEADING_CONTAINER}>
+                <VisibilityIcon size={24} color={colorScheme.ACCENT} />
+                <Text style={styles.HEADING_TEXT}>Visibility</Text>
+            </View>
+            
+            <View style={styles.CONTENT_CONTAINER}>
+                <Text style={styles.TIME_TEXT}>{visibility} km</Text>
+            </View>
+        </View>
+    );
+}
---
+++ Aurora/styles/components/visibilityCard.js
@@ -0,0 +1,50 @@
+import colors from '@/styles/global/colors';
+import { fontFamilies } from '@/styles/global/fonts';
+import { StyleSheet } from 'react-native';
+
+export default function cssAdapter(key){
+    const colorScheme = colors(key);
+    const styles = StyleSheet.create({
+        CARD_CONTAINER: {
+            backgroundColor: colorScheme.SECONDARY,
+            // borderWidth: 2,
+            // borderColor: colorScheme.SECONDARY,
+            alignItems: 'center',
+            borderRadius: 12,
+            height: 75,
+            width: '45%',
+            margin: 5
+        },
+
+        HEADING_CONTAINER: {
+            alignSelf: 'flex-start',
+            alignItems: 'center',
+            flexDirection: 'row',
+            marginBottom: 0,
+            margin: 5,
+        },
+
+        CONTENT_CONTAINER: {
+            justifyContent: 'center',
+            flexDirection: 'row',
+            alignItems: 'center',
+        },
+
+        HEADING_TEXT: {
+            fontFamily: fontFamilies.light,
+            color: colorScheme.ACCENT,
+            marginLeft: 8,
+            fontSize: 20,
+        },
+
+        TIME_TEXT: {
+            fontFamily: fontFamilies.medium,
+            color: colorScheme.ACCENT,
+            fontSize: 20,
+            margin: 5
+        },
+
+    });
+
+    return styles;
+}
