1b774023fbcd805f65bb595bce938eea2a32ebc3
Samvit Prakash <samvitprakash2004@gmail.com>
2026-02-02 15:06:11 +0200

    feat: add refresh functionality to SideButton and update props for custom actions

--- Aurora/components/side_button/SideButton.tsx
+++ Aurora/components/side_button/SideButton.tsx
@@ -1,12 +1,12 @@
 import { MaterialIcons } from '@expo/vector-icons';
+import { router } from 'expo-router';
 import { ComponentProps } from 'react';
 import { TouchableOpacity, View } from 'react-native';
-import { router } from 'expo-router';
 
-import cssAdapter from '@/styles/components/sideButton'
-import color from '@/styles/global/colors'
+import cssAdapter from '@/styles/components/sideButton';
+import color from '@/styles/global/colors';
 
-export default function SideButton({weatherVariable, page, icon, top, invert, right}:{weatherVariable:string, page:string, icon:string, top:string, invert?:boolean, right?:boolean}) {
+export default function SideButton({weatherVariable, page, icon, top, invert, right, custom}:{weatherVariable:string, page?:string, icon:string, top:string, invert?:boolean, right?:boolean, custom?: () => void }) {
     const styles = cssAdapter(weatherVariable, top);
     const colorScheme = color(weatherVariable);
 
@@ -21,7 +21,7 @@ export default function SideButton({weatherVariable, page, icon, top, invert, ri
 
 	return (
         <View style={[styles.SIDE_BUTTON_CONTAINER, styles[right ? 'SIDE_BUTTON_CONTAINER_RIGHT' : 'SIDE_BUTTON_CONTAINER_LEFT'], styles[invert ? 'COLOR_INVERT' : 'COLOR']]}>
-            <TouchableOpacity style={[styles.TOUCHABLE_OPACITY, styles[invert ? 'COLOR_INVERT' : 'COLOR']]} onPress={sideMenu}>
+            <TouchableOpacity style={[styles.TOUCHABLE_OPACITY, styles[invert ? 'COLOR_INVERT' : 'COLOR']]} onPress={!custom ? sideMenu : custom}>
                 <MaterialIcons name={icon as ComponentProps<typeof MaterialIcons>['name']} size={24} color={colorScheme[invert ?  'ACCENT' : 'SECONDARY']} />
             </TouchableOpacity>
         </View>
